d8e6b7aa647dc170c81ea751f71f5e16f2530f24
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-06 18:13:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,70 +9,31 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart (item) {
-  var price = Math.floor((Math.random() * 100) + 1);
-  var objectCart = {itemPrice: price, itemName: item}
-  cart.push(objectCart)
-  return (item +' has been added to your cart.');
+function addToCart(item) { 
+   var price = Math.floor((Math.random()*100) +1)
+   var ObjCart = ({itemName:item, itemPrice:price})
+   cart.push (ObjCart)
+   return `${item} has been added to your cart.`
+   
 }
 
-function viewCart() {
-  var cartString = "In your cart, you have "
-  
-  if (cart.length === 0) {
-    return "Your shopping cart is empty."
-    
-  } else if (cart.length === 1) {
-    return cartString + `${cart[0].itemName} at $${cart[0].itemPrice}.`
-    
-  } else if (cart.length === 2) {
-    return cartString + `${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
-    
-  } else if (cart.length>2){
-    
-    for(var i = 0; i < cart.length - 1; i++) {
-      cartString = cartString + `${cart[i].itemName} at $${cart[i].itemPrice}, `
-    }
-    
-    return cartString + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
-  }
+function viewCart(){
+var statement = 'In your cart, you have '
+if (cart.length === 0) {
+    return  'Your shopping cart is empty.'
 }
- 
-function total() {
-var total = 0
-for(var i = 0; i < cart.length; i++) {
-    total += cart[i].itemPrice;
-  }
-  return total;
+else if (cart.length === 1){
+    return statement + `${cart[0].itemName} at $${cart[0].itemPrice}.`
 }
-
-function removeFromCart(item) {
-
-
-  for (var i = 0; i < cart.length; i++){
-    if (item === cart[i].itemName) {
-      cart.splice(i, 1); 
-      return cart;
-    }
-  }
-
-  if (item != cart[item]) {
-    return "That item is not in your cart."
-  }
-
+else if (cart.length === 2) {
+  return statement + `${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
 }
-
-function placeOrder(cardNumber) {
-if(cardNumber) 
-  {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-    for (var i = cart.length; i>= 0; i--)
-    { cart.pop();} 
-  }
-  else {
-    return "Sorry, we don't have a credit card on file for you."
-  }
- 
+else if (cart.length>2) {
+for (var i=0; i<cart.length - 1; i++ ){
+statement = statement + `${cart[i].itemName} at $${cart[i].itemPrice}, `
+}
+return statement + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
+}
 }
 
   
